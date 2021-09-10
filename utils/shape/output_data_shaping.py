789b4c621725f10719b4e510ed369baed3de4d52
kazuki <kaz.tokyo.tech20@gmail.com>
2021-09-10 11:52:05 +0900

    出力データ用の整形用関数の追加

---
+++ utils/shape/output_data_shaping.py
@@ -0,0 +1,66 @@
+import re
+
+
+import pandas as pd
+
+
+def shape(data: dict):
+    '''出力するcsvファイルとして望ましい形に整形する'''
+    res_data: dict = {}
+    # 政令指定都市かどうかを判別するためのデータ
+    PATH = 'data/Ordinance_designated_city.csv'
+    Ordinance_designated_city_csv = pd.read_csv(PATH)
+    # データをコピー
+    res_data['original'] = data['original']
+    res_data['prefacture'] = data['prefacture']
+    # 最終出力結果に合わせて整形 市区町村郡
+    address1: list = []
+    for index in range(len(data['city'])):
+        if data['city'][index] in list(Ordinance_designated_city_csv):
+            # 政令指定都市ならば
+            address1.append(data['city'][index] + data['town'][index])
+            data['town'][index] = ''
+        elif re.search('郡$', data['city'][index]):
+            # 郡ならば
+            address1.append(data['city'][index] + data['town'][index])
+            data['town'][index] = ''
+        else:
+            address1.append(data['city'][index])
+    res_data['address1'] = address1
+    # 町域について
+    address2: list = []
+    for index in range(len(data['town'])):
+        if re.search('[0-9 -]', data['town'][index]) or re.search('[0-9 -]', data['district'][index]):
+            # 不正な文字が存在する caution
+            address2.append(data['town'][index] + data['district'][index])
+        else:
+            address2.append(data['town'][index] + data['district'][index])
+    res_data['address2'] = address2
+    # 番地について
+    address3: list = []
+    for index in range(len(data['house_number'])):
+        if re.search('(([0-9]+)-)* [0-9]+', data['house_number'][index]):
+            address3.append(data['house_number'][index])
+        else:
+            # 不正な文字が存在 caution
+            address3.append(data['house_number'][index])
+    res_data['address3'] = address3
+    # 建物名
+    address4: list = []
+    for index in range(len(data['building_info'])):
+        if data['special_characters'][index]:
+            # special_charactersのセルが空ではない caution
+            address4.append(data['special_characters'][index] + data['building_info'][index])
+        else:
+            # special_charactersが空
+            address4.append(data['building_info'][index])
+    res_data['address4'] = address4
+    # 部屋番号
+    address5: list = []
+    for index in range(len(data['building_detail_info'])):
+        address5.append(data['building_detail_info'][index])
+    res_data['address5'] = address5
+    # caution
+    res_data['caution'] = data['caution']
+    # 返値
+    return res_data
