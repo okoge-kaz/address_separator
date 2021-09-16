f511fe60f2b069e0aeb927b2fdf2088b4aae8388
kazuki <kaz.tokyo.tech20@gmail.com>
2021-09-16 14:50:03 +0900

    fix condition error

--- utils/extract/town.py
+++ utils/extract/town.py
@@ -10,24 +10,32 @@ def extract_town(non_city_address_data: list):
             if len(town_address_data) > index + 1:
                 # index out of rangeを避けるため
                 non_town_address_data.append(town_address_data[index + 1:])
+            else:
+                non_town_address_data.append('')
         elif "郡" in town_address_data:
             index: int = town_address_data.find("郡")
             towns.append(town_address_data[0:index + 1])
             if len(town_address_data) > index + 1:
                 # index out of rangeを避けるため
                 non_town_address_data.append(town_address_data[index + 1:])
+            else:
+                non_town_address_data.append('')
         elif "町" in town_address_data:
             index: int = town_address_data.find("町")
             towns.append(town_address_data[0:index + 1])
             if len(town_address_data) > index + 1:
                 # index out of rangeを避けるため
                 non_town_address_data.append(town_address_data[index + 1:])
+            else:
+                non_town_address_data.append('')
         elif "村" in town_address_data:
             index: int = town_address_data.find("村")
             towns.append(town_address_data[0:index + 1])
             if len(town_address_data) > index + 1:
                 # index out of rangeを避けるため
                 non_town_address_data.append(town_address_data[index + 1:])
+            else:
+                non_town_address_data.append('')
         else:
             towns.append("")
             non_town_address_data.append(town_address_data)
