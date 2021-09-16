8088a2343da2892ce55b7d8a9d28f6682e5545b0
kazuki <kaz.tokyo.tech20@gmail.com>
2021-09-16 14:49:17 +0900

    fix typo

--- utils/extract/detail/check.py
+++ utils/extract/detail/check.py
@@ -4,7 +4,7 @@ import re
 def check(data: dict):
     '''cautionの配列を生成する'''
     def check_vaild_word_or_not(index: int):
-        if re.search('^([0-9 ぁ-んァ-ヶｱ-ﾝﾞﾟ一-龠])+$', data['invalid'][index]) is not None:
+        if re.search('^([0-9ぁ-んァ-ヶｱ-ﾝﾞﾟ一-龠])+$', data['invalid'][index]) is not None:
             mapping_arithmetic_number_to_japanese_number: dict = {
                 '1': '一', '2': '二', '3': '三', '4': '四', '5': '五', '6': '六', '7': '七', '8': '八', '9': '九'}
             numbers: list = [str(i) for i in range(1, 10)]
@@ -19,7 +19,7 @@ def check(data: dict):
             return ""
 
     caution: list = []
-    for index in range(len(data['original'])):
+    for index in range(len(data['invalid'])):
         response: str = check_vaild_word_or_not(index)
         if data['invalid'][index] == "":
             caution.append('')
