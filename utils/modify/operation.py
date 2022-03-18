6460a3bd27be7da75aedc576762ce50b35bf78fb
kazuki <kaz.tokyo.tech20@gmail.com>
2022-03-18 22:28:15 +0900

    refactor: file division

--- utils/modify/operation.py
+++ utils/modify/operation.py
@@ -1,6 +1,7 @@
 from utils.modify.handleMiscellaneousBug import hadle_miscellaneous_bugs
 from utils.modify.modifyBuildingDataFieldAfterSplit import modify_building_dat_field
 from utils.modify.modifySpcialPlaceNameAfterSplit import modify_special_place_name
+from utils.modify.reSplitByEveryFieldAfterSplit import re_split_by_every_fields
 
 
 def post_process(splitedAddressDataDictionarys: dict[str, list[str]]):
@@ -11,3 +12,4 @@ def post_process(splitedAddressDataDictionarys: dict[str, list[str]]):
     modify_special_place_name(splitedAddressDataDictionarys)
     modify_building_dat_field(splitedAddressDataDictionarys)
     hadle_miscellaneous_bugs(splitedAddressDataDictionarys)
+    re_split_by_every_fields(splitedAddressDataDictionarys)
