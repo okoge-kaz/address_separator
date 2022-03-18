5e137ac4d827d77eee557399fbfd5fae4e396dd0
kazuki <kaz.tokyo.tech20@gmail.com>
2022-03-18 17:45:11 +0900

    refactor: add japanese style number fix

---
+++ utils/preprocess/pre/convertJapaneseStyleNumberToAlabicNumber.py
@@ -0,0 +1,54 @@
+import re
+
+
+def convertJapaneseStyleNumberToAlabicNumber(address_data: str) -> str:
+    """
+    args: address_data: str (example: 東京都港区元麻布1-六-9)
+    return: formatted_address_data: str (example: 東京都港区元麻布1-6-9)
+    """
+
+    JAPANSE_STYLE_NUMBER: list = ["一", "二", "三", "四", "五", "六", "七", "八", "九", "〇"]
+    MAPPING_DICTIONARY: dict = {
+        "１": "1",
+        "２": "2",
+        "３": "3",
+        "４": "4",
+        "５": "5",
+        "６": "6",
+        "７": "7",
+        "８": "8",
+        "９": "9",
+        "０": "0",
+    }
+    DICTIONARY_JAPANESE_STYLE_NUMBER: dict = {
+        "一": "1",
+        "二": "2",
+        "三": "3",
+        "四": "4",
+        "五": "5",
+        "六": "6",
+        "七": "7",
+        "八": "8",
+        "九": "9",
+        "十": "10",
+        "〇": "0",
+    }
+
+    response: str = ""
+
+    for character in address_data:
+
+        _formatted_character: str = ""
+        if character in JAPANSE_STYLE_NUMBER:
+            # 漢数字を算用数字に変換
+            _formatted_character += DICTIONARY_JAPANESE_STYLE_NUMBER[character]
+        else:
+            _formatted_character += character
+
+        if re.search("[０-９]", _formatted_character):
+            response += MAPPING_DICTIONARY[_formatted_character]
+        else:
+            response += _formatted_character
+
+    formatted_address_data: str = response
+    return formatted_address_data
