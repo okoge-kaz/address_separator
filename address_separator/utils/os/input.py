35752922cf05a4a326dd72397cc443821aa3304d
kazuki <kaz.tokyo.tech20@gmail.com>
2022-04-11 20:55:37 +0900

    refactor: format and add docstring

--- address_separator/utils/os/input.py
+++ address_separator/utils/os/input.py
@@ -3,15 +3,26 @@ import os
 import pandas as pd
 
 
-def get_input_csv_data(Relative_PATH: str = "/input/input.xlsx") -> pd.DataFrame:
+def get_input_csv_data(relative_path: str = "/input/input.xlsx") -> pd.DataFrame:
     """
-    args: relative_path: str
-    return: csv_data: pd.DataFrame
+    read input file: 入力ファイルの読み込み
+
+    Parameters
+    ----------
+    relative_path : str
+        相対パス
+
+    Returns
+    -------
+    pd.DataFrame
+        入力データ
     """
-    Current_Path = os.getcwd()
-    PATH = Current_Path + Relative_PATH
+    CURRENT_PATH: str = os.getcwd()
+    PATH = CURRENT_PATH + relative_path
+
     try:
         CSV_DATA = pd.read_excel(PATH)
     except FileNotFoundError:
-        CSV_DATA = pd.read_csv(Current_Path + "/input/input.csv")
+        CSV_DATA = pd.read_csv(CURRENT_PATH + "/input/input.csv")
+
     return CSV_DATA
--- address_separator/utils/os/output.py
+++ address_separator/utils/os/output.py
@@ -1,12 +1,26 @@
-import pandas as pd
 import os
 
-def create_output_data(formated_dictionary_data: dict, Relative_PATH: str = "/output/output.csv") -> None:
+import pandas as pd
+
+
+def create_output_data(formated_dictionary_data: dict, relative_path: str = "/output/output.csv") -> None:
     """
-    args: formated_dictionary_data: dict
-    return: void
+    create output file: 出力ファイルの作成
+
+    Parameters
+    ----------
+    formated_dictionary_data : dict
+        整形済みの辞書データ
+    Relative_PATH : str
+        相対パス
+
+    Returns
+    -------
+        void
     """
-    Current_PATH = os.getcwd()
-    PATH = Current_PATH + Relative_PATH
-    output_data = pd.DataFrame(formated_dictionary_data)
+    CURRENT_PATH: str = os.getcwd()
+    PATH = CURRENT_PATH + relative_path
+
+    output_data: pd.DataFrame = pd.DataFrame(formated_dictionary_data)
+
     output_data.to_csv(PATH, encoding="utf-8_sig")
