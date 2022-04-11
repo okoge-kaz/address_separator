cef958dd50a4474d6bee61140754598fe5ee0487
kazuki <kaz.tokyo.tech20@gmail.com>
2022-04-11 21:55:14 +0900

    add type annotation and format

--- address_separator/utils/modify/modifySpecialPlaceNameAfterSplit.py
+++ address_separator/utils/modify/modifySpecialPlaceNameAfterSplit.py
@@ -1,9 +1,9 @@
 import re
 
 
-def modify_special_place_name(splittedAddressDataDictionaries: dict[str, list[str]]):
+def modify_special_place_name(splitted_address_data_dictionaries: dict[str, list[str]]) -> None:
     """
-    args: splittedAddressDataDictionaries (すでに分割された住所データの辞書型, 出力用には整形済み)
+    args: splitted_address_data_dictionaries (すでに分割された住所データの辞書型, 出力用には整形済み)
     return: void
 
     各ケースごとに処理を行う
@@ -12,110 +12,110 @@ def modify_special_place_name(splittedAddressDataDictionaries: dict[str, list[st
 
     """
 
-    DATA_SIZE: int = len(splittedAddressDataDictionaries["address3"])
+    DATA_SIZE: int = len(splitted_address_data_dictionaries["address3"])
 
     # 一宮
     for index in range(DATA_SIZE):
         if (
-            splittedAddressDataDictionaries["address3"][index] == "1"
-            and splittedAddressDataDictionaries["address4"][index] == "宮"
+            splitted_address_data_dictionaries["address3"][index] == "1"
+            and splitted_address_data_dictionaries["address4"][index] == "宮"
         ):
-            if splittedAddressDataDictionaries["address2"][index] != "":
-                splittedAddressDataDictionaries["error1"][index] += "ERROR: 町域と判断される文字列が２つあります。自動整形システムは正しく動作しません。  "
+            if splitted_address_data_dictionaries["address2"][index] != "":
+                splitted_address_data_dictionaries["error1"][index] += "ERROR: 町域と判断される文字列が２つあります。自動整形システムは正しく動作しません。  "
             else:
-                splittedAddressDataDictionaries["address2"][index] = "一宮"
-                splittedAddressDataDictionaries["address3"][index] = splittedAddressDataDictionaries["address5"][index]
-                splittedAddressDataDictionaries["address4"][index] = ""
-                splittedAddressDataDictionaries["address5"][index] = ""
+                splitted_address_data_dictionaries["address2"][index] = "一宮"
+                splitted_address_data_dictionaries["address3"][index] = splitted_address_data_dictionaries["address5"][index]
+                splitted_address_data_dictionaries["address4"][index] = ""
+                splitted_address_data_dictionaries["address5"][index] = ""
 
     # 一宮徳谷
     for index in range(DATA_SIZE):
         if (
-            splittedAddressDataDictionaries["address3"][index] == "1"
-            and splittedAddressDataDictionaries["address4"][index] == "宮徳谷"
+            splitted_address_data_dictionaries["address3"][index] == "1"
+            and splitted_address_data_dictionaries["address4"][index] == "宮徳谷"
         ):
-            if splittedAddressDataDictionaries["address2"][index] != "":
-                splittedAddressDataDictionaries["error1"][index] += "ERROR: 町域と判断される文字列が２つあります。自動整形システムは正しく動作しません。  "
+            if splitted_address_data_dictionaries["address2"][index] != "":
+                splitted_address_data_dictionaries["error1"][index] += "ERROR: 町域と判断される文字列が２つあります。自動整形システムは正しく動作しません。  "
             else:
-                splittedAddressDataDictionaries["address2"][index] = "一宮徳谷"
-                splittedAddressDataDictionaries["address3"][index] = splittedAddressDataDictionaries["address5"][index]
-                splittedAddressDataDictionaries["address4"][index] = ""
-                splittedAddressDataDictionaries["address5"][index] = ""
+                splitted_address_data_dictionaries["address2"][index] = "一宮徳谷"
+                splitted_address_data_dictionaries["address3"][index] = splitted_address_data_dictionaries["address5"][index]
+                splitted_address_data_dictionaries["address4"][index] = ""
+                splitted_address_data_dictionaries["address5"][index] = ""
 
     # 五台山
     for index in range(DATA_SIZE):
         if (
-            splittedAddressDataDictionaries["address3"][index] == "5"
-            and splittedAddressDataDictionaries["address4"][index] == "台山"
+            splitted_address_data_dictionaries["address3"][index] == "5"
+            and splitted_address_data_dictionaries["address4"][index] == "台山"
         ):
-            if splittedAddressDataDictionaries["address2"][index] != "":
-                splittedAddressDataDictionaries["error1"][index] += "ERROR: 町域と判断される文字列が２つあります。自動整形システムは正しく動作しません。  "
+            if splitted_address_data_dictionaries["address2"][index] != "":
+                splitted_address_data_dictionaries["error1"][index] += "ERROR: 町域と判断される文字列が２つあります。自動整形システムは正しく動作しません。  "
             else:
-                splittedAddressDataDictionaries["address2"][index] = "五台山"
-                splittedAddressDataDictionaries["address3"][index] = splittedAddressDataDictionaries["address5"][index]
-                splittedAddressDataDictionaries["address4"][index] = ""
-                splittedAddressDataDictionaries["address5"][index] = ""
+                splitted_address_data_dictionaries["address2"][index] = "五台山"
+                splitted_address_data_dictionaries["address3"][index] = splitted_address_data_dictionaries["address5"][index]
+                splitted_address_data_dictionaries["address4"][index] = ""
+                splitted_address_data_dictionaries["address5"][index] = ""
 
     # 五台山〜
     for index in range(DATA_SIZE):
-        if splittedAddressDataDictionaries["address3"][index] == "5" and re.search(
-            "^台山.+", splittedAddressDataDictionaries["address4"][index]
+        if splitted_address_data_dictionaries["address3"][index] == "5" and re.search(
+            "^台山.+", splitted_address_data_dictionaries["address4"][index]
         ):
-            if splittedAddressDataDictionaries["address2"][index] != "":
-                splittedAddressDataDictionaries["error1"][index] += "ERROR: 町域と判断される文字列が２つあります。自動整形システムは正しく動作しません。  "
+            if splitted_address_data_dictionaries["address2"][index] != "":
+                splitted_address_data_dictionaries["error1"][index] += "ERROR: 町域と判断される文字列が２つあります。自動整形システムは正しく動作しません。  "
             else:
-                splittedAddressDataDictionaries["address2"][index] = ""
-                splittedAddressDataDictionaries["address3"][index] = ""
-                splittedAddressDataDictionaries["address4"][index] = (
-                    "五" + splittedAddressDataDictionaries["address4"][index]
+                splitted_address_data_dictionaries["address2"][index] = ""
+                splitted_address_data_dictionaries["address3"][index] = ""
+                splitted_address_data_dictionaries["address4"][index] = (
+                    "五" + splitted_address_data_dictionaries["address4"][index]
                 )
 
     # 三谷
     for index in range(DATA_SIZE):
         if (
-            splittedAddressDataDictionaries["address3"][index] == "3"
-            and splittedAddressDataDictionaries["address4"][index] == "谷"
+            splitted_address_data_dictionaries["address3"][index] == "3"
+            and splitted_address_data_dictionaries["address4"][index] == "谷"
         ):
-            if splittedAddressDataDictionaries["address2"][index] != "":
-                splittedAddressDataDictionaries["error1"][index] += "ERROR: 町域と判断される文字列が２つあります。自動整形システムは正しく動作しません。  "
+            if splitted_address_data_dictionaries["address2"][index] != "":
+                splitted_address_data_dictionaries["error1"][index] += "ERROR: 町域と判断される文字列が２つあります。自動整形システムは正しく動作しません。  "
             else:
-                splittedAddressDataDictionaries["address2"][index] = "三谷"
-                splittedAddressDataDictionaries["address3"][index] = splittedAddressDataDictionaries["address5"][index]
-                splittedAddressDataDictionaries["address4"][index] = ""
-                splittedAddressDataDictionaries["address5"][index] = ""
+                splitted_address_data_dictionaries["address2"][index] = "三谷"
+                splitted_address_data_dictionaries["address3"][index] = splitted_address_data_dictionaries["address5"][index]
+                splitted_address_data_dictionaries["address4"][index] = ""
+                splitted_address_data_dictionaries["address5"][index] = ""
 
     # 三原
     for index in range(DATA_SIZE):
         if (
-            splittedAddressDataDictionaries["address3"][index] == "3"
-            and splittedAddressDataDictionaries["address4"][index] == "原"
+            splitted_address_data_dictionaries["address3"][index] == "3"
+            and splitted_address_data_dictionaries["address4"][index] == "原"
         ):
-            if splittedAddressDataDictionaries["address2"][index] != "":
-                splittedAddressDataDictionaries["error1"][index] += "ERROR: 町域と判断される文字列が２つあります。自動整形システムは正しく動作しません。  "
+            if splitted_address_data_dictionaries["address2"][index] != "":
+                splitted_address_data_dictionaries["error1"][index] += "ERROR: 町域と判断される文字列が２つあります。自動整形システムは正しく動作しません。  "
             else:
-                splittedAddressDataDictionaries["address2"][index] = "三原"
-                splittedAddressDataDictionaries["address3"][index] = splittedAddressDataDictionaries["address5"][index]
-                splittedAddressDataDictionaries["address4"][index] = ""
-                splittedAddressDataDictionaries["address5"][index] = ""
+                splitted_address_data_dictionaries["address2"][index] = "三原"
+                splitted_address_data_dictionaries["address3"][index] = splitted_address_data_dictionaries["address5"][index]
+                splitted_address_data_dictionaries["address4"][index] = ""
+                splitted_address_data_dictionaries["address5"][index] = ""
 
     # 三重城
     for index in range(DATA_SIZE):
-        if splittedAddressDataDictionaries["address3"][index] == "":
+        if splitted_address_data_dictionaries["address3"][index] == "":
             continue
         if (
-            re.search("^重城", splittedAddressDataDictionaries["address4"][index])
-            and splittedAddressDataDictionaries["address3"][index][-1] == "3"
+            re.search("^重城", splitted_address_data_dictionaries["address4"][index])
+            and splitted_address_data_dictionaries["address3"][index][-1] == "3"
         ):
-            if len(splittedAddressDataDictionaries["address3"][index]) < 2:
+            if len(splitted_address_data_dictionaries["address3"][index]) < 2:
                 continue
-            splittedAddressDataDictionaries["address3"][index] = splittedAddressDataDictionaries["address3"][index][:-2]
-            splittedAddressDataDictionaries["address4"][index] = "三" + splittedAddressDataDictionaries["address4"][index]
+            splitted_address_data_dictionaries["address3"][index] = splitted_address_data_dictionaries["address3"][index][:-2]
+            splitted_address_data_dictionaries["address4"][index] = "三" + splitted_address_data_dictionaries["address4"][index]
 
     # 町屋
     for index in range(DATA_SIZE):
         if (
-            splittedAddressDataDictionaries["address1"][index] == "町"
-            and splittedAddressDataDictionaries["address2"][index] == "屋"
+            splitted_address_data_dictionaries["address1"][index] == "町"
+            and splitted_address_data_dictionaries["address2"][index] == "屋"
         ):
-            splittedAddressDataDictionaries["address2"][index] = "町屋"
-            splittedAddressDataDictionaries["address1"][index] = ""
+            splitted_address_data_dictionaries["address2"][index] = "町屋"
+            splitted_address_data_dictionaries["address1"][index] = ""
