d9588efa103a6004927ba0db43cf322243dfd729
kazuki <kaz.tokyo.tech20@gmail.com>
2022-04-11 22:25:54 +0900

    refactor

--- address_separator/main.py
+++ address_separator/main.py
@@ -1,24 +1,28 @@
 import time
 
+import pandas as pd
+
 import utils.extract.detail.check.detail_data_check
-import utils.shape.output_data_shaping
 from utils.modify.operation import post_process
 from utils.os.input import get_input_csv_data
 from utils.os.output import create_output_data
 from utils.preprocess.operation import pretreatment
+from utils.reformat.output_data_shaping import formart_for_output
 from utils.split.splitByAddressField import split_by_address_field
 
 
 def main():
 
-    CSV_DATA = get_input_csv_data()
+    CSV_DATA: pd.DataFrame = get_input_csv_data()
 
     formatted_address_data_array: list[str] = pretreatment(CSV_DATA)
 
-    splittedAddressDataDictionaries = split_by_address_field(formatted_address_data_array, CSV_DATA)
+    splitted_address_data_dictionaries: dict[str, list[str]] = split_by_address_field(
+        formatted_address_data_array, CSV_DATA
+    )
 
     # 出力形式用にデータを再整形
-    splittedAddressDataDictionaries = utils.shape.output_data_shaping.shape(splittedAddressDataDictionaries)
+    splittedAddressDataDictionaries = formart_for_output(splitted_address_data_dictionaries)
 
     # 特殊な町域などの経験則的修正
     post_process(splittedAddressDataDictionaries)
