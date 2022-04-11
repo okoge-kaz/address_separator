6180e223d00d251705a7584154926d2e26988482
kazuki <kaz.tokyo.tech20@gmail.com>
2022-04-11 21:35:33 +0900

    add docstring and separate code into two files

---
+++ address_separator/utils/split/pullOutDistrictField.py
@@ -0,0 +1,17 @@
+def pull_out_district_field(non_town_address_data: str) -> tuple[str, str]:
+    """_summary_
+
+    Args:
+        non_town_address_data (str): 
+
+    Returns:
+        tuple[str, str]:
+    """
+    DISTRICT_NUMBERS: list[str] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    # 数字が現れるまでを記録する
+
+    for index in range(len(non_town_address_data)):
+        if non_town_address_data[index] in DISTRICT_NUMBERS:
+            return (non_town_address_data[0:index], non_town_address_data[index:])
+
+    return ("", non_town_address_data)
