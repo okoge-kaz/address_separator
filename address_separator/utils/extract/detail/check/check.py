8bffa4fbecb7d3ccad452bdad9b40c6ce4f9029d
haruboring <raiu_hk@icloud.com>
2022-05-08 18:25:07 +0900

    refactoring

--- address_separator/utils/extract/detail/check/check.py
+++ address_separator/utils/extract/detail/check/check.py
@@ -3,24 +3,24 @@ from __future__ import annotations
 import re
 
 
-def check(splitted_address_data_dictionaries: dict, others_tail: list[str]) -> list[str]:
+def check(data: dict, others_tail: list[str]) -> list[str]:
     """
     cautionの配列を生成する
     """
 
-    def check_vaild_word_or_not(index: int):
-        if re.search("^([0-9ぁ-んァ-ヶｱ-ﾝﾞﾟ一-龠-])+$", splitted_address_data_dictionaries["invalid"][index]) is not None:
+    def check_valid_word_or_not(index: int):
+        if re.search("^([0-9ぁ-んァ-ヶｱ-ﾝﾞﾟ一-龠-])+$", data["invalid"][index]) is not None:
             # 愛知県名古屋市天白区天白町平針黒石２８４５平針住宅14-24
-            if re.search("^[0-9]{2,}", splitted_address_data_dictionaries["invalid"][index]) is not None:
-                match = re.search("^[0-9]{2,}", splitted_address_data_dictionaries["invalid"][index])
+            if re.search("^[0-9]{2,}", data["invalid"][index]) is not None:
+                match = re.search("^[0-9]{2,}", data["invalid"][index])
                 assert match is not None
-                string = splitted_address_data_dictionaries["invalid"][index]
+                invalid_word = data["invalid"][index]
                 if index is not None:
-                    others_tail[index] = string[match.end() :] + splitted_address_data_dictionaries["house_number"][index] + others_tail[index]
-                    splitted_address_data_dictionaries["house_number"][index] = string[0 : match.end()]
-                    splitted_address_data_dictionaries["invalid"][index] = ""
+                    others_tail[index] = invalid_word[match.end() :] + data["house_number"][index] + others_tail[index]
+                    data["house_number"][index] = invalid_word[0 : match.end()]
+                    data["invalid"][index] = ""
 
-            mapping_arithmetic_number_to_japanese_number: dict = {
+            MAPPING_ARITHMETIC_NUMBER_TO_JAPANESE_NUMBER: dict = {
                 "1": "一",
                 "2": "二",
                 "3": "三",
@@ -33,9 +33,9 @@ def check(splitted_address_data_dictionaries: dict, others_tail: list[str]) -> l
             }
             numbers: list = [str(i) for i in range(1, 10)]
             res: str = ""
-            for char in splitted_address_data_dictionaries["invalid"][index]:
+            for char in data["invalid"][index]:
                 if char in numbers:
-                    res = res + mapping_arithmetic_number_to_japanese_number[char]
+                    res = res + MAPPING_ARITHMETIC_NUMBER_TO_JAPANESE_NUMBER[char]
                 else:
                     res = res + char
             return res
@@ -43,15 +43,15 @@ def check(splitted_address_data_dictionaries: dict, others_tail: list[str]) -> l
             return ""
 
     caution: list = []
-    for index in range(len(splitted_address_data_dictionaries["invalid"])):
-        response: str = check_vaild_word_or_not(index)
-        if splitted_address_data_dictionaries["invalid"][index] == "":
+    for index in range(len(data["invalid"])):
+        response: str = check_valid_word_or_not(index)
+        if data["invalid"][index] == "":
             caution.append("")
         elif response == "":
             # 不正な文字列
             caution.append("ERROR: データは、整形不可能な状態です。自動整形システムは正しく動作しません。この行の全ての結果を確認することを推奨します。  ")
         else:
             caution.append("")
-            splitted_address_data_dictionaries["invalid"][index] = ""
-            splitted_address_data_dictionaries["district"][index] += response
+            data["invalid"][index] = ""
+            data["district"][index] += response
     return caution
