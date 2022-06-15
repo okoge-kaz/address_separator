e124f0cc97b4aaa30a83f3ecbf1ce0b7bf253744
haruboting <raiu_hk@icloud.com>
2022-06-16 01:32:02 +0900

    rename column names to japanese

--- address_separator/utils/os/output.py
+++ address_separator/utils/os/output.py
@@ -21,6 +21,21 @@ def create_output_data(formated_dictionary_data: dict, relative_path: str = "/ou
     CURRENT_PATH: str = os.getcwd()
     PATH = CURRENT_PATH + relative_path
 
-    output_data: pd.DataFrame = pd.DataFrame(formated_dictionary_data)
+    en_output_data: pd.DataFrame = pd.DataFrame(formated_dictionary_data)
+    # 列名を日本語に変更
+    jp_output_data: pd.DataFrame = en_output_data.rename(
+        columns={
+            "original": "元データ",
+            "prefecture": "都道府県",
+            "address1": "市や特別区",
+            "address2": "町村、区および字",
+            "address3": "番地",
+            "address4": "建物名",
+            "address5": "建物情報",
+            "error1": "深刻な警告",
+            "error2": "警告",
+            "caution": "注意",
+        }
+    )
 
-    output_data.to_csv(PATH, encoding="utf-8_sig")
+    jp_output_data.to_csv(PATH, encoding="utf-8_sig")
